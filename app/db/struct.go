3e51a047207f9247bfde3075ef791ec75c09f82f
Faisal Budiono <ucul.project@gmail.com>
2024-08-06 14:51:15 +0700

    feat(resource): add dummy resource page for POC

---
+++ app/db/struct.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"time"
+)
+
+type ResourceField struct {
+	Id            int
+	FieldName     string
+	ResourceRefId int
+	Type          string
+	IsNullable    bool
+	IsArray       bool
+	Default       string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+type Resource struct {
+	Id        int
+	Name      string
+	Field     []ResourceField
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,57 @@
 package main
 
 import (
+	"FaisalBudiono/see-this/app/db"
+	"FaisalBudiono/see-this/app/httpren"
 	"FaisalBudiono/see-this/view"
 	"net/http"
 
-	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+var (
+	tempResources = make([]db.Resource, 0)
+	id            = 2
+)
+
+func makeDummyResource() []db.Resource {
+	return []db.Resource{
+		{
+			Id:   1,
+			Name: "asd",
+		},
+		{
+			Id:   2,
+			Name: "kambing",
+		},
+	}
+}
 
 func main() {
 	e := echo.New()
 
+	tempResources = append(tempResources, makeDummyResource()...)
+
 	e.Static("dist", "dist")
 
+	e.Use(middleware.Logger())
+
 	e.GET("/", func(c echo.Context) error {
-		return Render(c, http.StatusOK, view.Kuda("asd"))
+		return httpren.Render(c, http.StatusOK, view.IndexPage(tempResources))
 	})
-	e.Logger.Fatal(e.Start(":8000"))
-}
 
-func Render(ctx echo.Context, statusCode int, t templ.Component) error {
-	buf := templ.GetBuffer()
-	defer templ.ReleaseBuffer(buf)
+	e.POST("/resources", func(c echo.Context) error {
+		name := c.FormValue("name")
 
-	if err := t.Render(ctx.Request().Context(), buf); err != nil {
-		return err
-	}
+		id += 1
+		tempResources = append(tempResources, db.Resource{
+			Id:   id,
+			Name: name,
+		})
 
-	return ctx.HTML(statusCode, buf.String())
+		return httpren.Render(c, http.StatusOK, view.ResourceList(tempResources))
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
